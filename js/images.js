603cec88712c202369376f181451c27ca1ee6e44
Rafi Miller <rafi.miller@vinciworks.com>
2019-08-15 01:33:42 +0300

    use public-url for deploy

--- js/images.js
+++ js/images.js
@@ -3,14 +3,14 @@ import * as png from '../img/*.png';
 
 const images = {};
 
-const path = window.location.href.split('index.html')[0].split(/\/$/)[0];
+// const path = window.location.href.split('index.html')[0].split(/\/$/)[0];
 
 Object.keys(jpg).forEach(k => {
-  images[k + '.jpg'] = path + jpg[k];
+  images[k + '.jpg'] = jpg[k];
 });
 
 Object.keys(png).forEach(k => {
-  images[k + '.png'] = path + png[k];
+  images[k + '.png'] = png[k];
 });
 
 export default images;
